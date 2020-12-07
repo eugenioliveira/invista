6e7ba20734c5ac46f7e6e156606280246ef8853f
Eugenio Oliveira <geninoliveira@gmail.com>
2020-12-07 15:54:36 -0300

    fixed login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -33,13 +33,23 @@
                     @csrf
 
                     <div>
-                        <x-jet-label for="email" value="E-mail"/>
-                        <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus/>
+                        <x-input
+                            label="E-mail"
+                            name="email"
+                            class="mt-1 w-full"
+                            value="{{ old('email') }}"
+                            required autofocus
+                        />
                     </div>
 
                     <div class="mt-4">
-                        <x-jet-label for="password" value="Senha"/>
-                        <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password"/>
+                        <x-input
+                            label="Senha"
+                            type="password"
+                            name="password"
+                            class="mt-1 w-full"
+                            required autocomplete="current-password"
+                        />
                     </div>
 
                     <div class="flex items-center justify-end mt-4">
