db79f244313617d6dc159be37842e2feaf63e046
Eugenio Oliveira <geninoliveira@gmail.com>
2020-12-18 11:54:27 -0300

    moving from address entity

---
+++ database/migrations/2020_12_18_115046_create_addresses_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAddressesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')
+                ->references('id')
+                ->on('users')
+                ->cascadeOnUpdate()
+                ->cascadeOnDelete();
+            $table->string('street');
+            $table->string('number');
+            $table->string('apt_room')->nullable();
+            $table->string('neighbourhood');
+            $table->string('city');
+            $table->string('state');
+            $table->string('postal_code');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('addresses');
+    }
+}
