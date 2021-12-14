120836691a42e0bd3d68efc6494f554cf9c352d3
Eugenio Oliveira <geninoliveira@gmail.com>
2021-12-14 11:32:13 -0300

    Ajustes dos testes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -28,8 +28,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if (env('APP_ENV') !== 'local') {
-            \URL::forceScheme('https');
-        }
+        \URL::forceScheme('https');
     }
 }
