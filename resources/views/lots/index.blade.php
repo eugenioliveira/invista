e60f273697b06b174112b289546949533a2ec536
Eugenio Oliveira <geninoliveira@gmail.com>
2021-12-15 12:02:21 -0300

    Desativa opção de importação

--- resources/views/lots/index.blade.php
+++ resources/views/lots/index.blade.php
@@ -10,8 +10,8 @@
             @if(\Auth::user()->isAdmin())
                 <div>
                     <x-button-link href="{{ route('lot.create', $allotment->id) }}">Criar novo</x-button-link>
-                    <x-button-link href="{{ route('lots.import', $allotment->id) }}" class="ml-3">Importar
-                    </x-button-link>
+                    <!--<x-button-link href="{{ route('lots.import', $allotment->id) }}" class="ml-3">Importar
+                    </x-button-link>-->
                 </div>
             @endif
         </div>
