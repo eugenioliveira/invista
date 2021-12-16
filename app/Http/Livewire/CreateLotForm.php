422f0f6c4138f0cd4178072e86249ee583e5d4f1
Eugenio Oliveira <geninoliveira@gmail.com>
2021-12-16 11:21:34 -0300

    Corrige problema com campo curvatura

--- app/Http/Livewire/CreateLotForm.php
+++ app/Http/Livewire/CreateLotForm.php
@@ -96,7 +96,7 @@ class CreateLotForm extends Component
             'lot.number' => ['required', 'numeric'],
             'lot.price' => ['required', 'regex:/^[1-9]\d*(\.\d{3})?(\,\d{1,2})?$/'],
             'lot.total' => ['required', 'regex:/^[1-9]\d*(\,\d{1,2})?$/'],
-            'lot.curve' => ['nullable', 'regex:/^[1-9]\d*(\,\d{1,2})?$/'],
+            'lot.curve' => ['nullable', 'regex:/^[0-9]\d*(\,\d{1,2})?$/'],
             'lot.front' => ['required', 'regex:/^[1-9]\d*(\,\d{1,2})?$/'],
             'lot.back' => ['required', 'regex:/^[1-9]\d*(\,\d{1,2})?$/'],
             'lot.right' => ['required', 'regex:/^[1-9]\d*(\,\d{1,2})?$/'],
